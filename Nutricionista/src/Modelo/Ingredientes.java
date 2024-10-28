4c2e54dfb208ab42382a764a8a709d4b82059df6
int-ExIT <eittien@gmail.com>
2024-10-28 18:09:22 -0300

    .

--- Nutricionista/src/Modelo/Ingredientes.java
+++ Nutricionista/src/Modelo/Ingredientes.java
@@ -1,23 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelo;
 
 import Persistencia.Conexion;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import java.util.Objects;
+
 import javax.swing.JOptionPane;
 
 
 public class Ingredientes {
+
     private String nombre;
-    private float cantidadCaloriasEn100gs;
+    private float caloriasCda100g;
     
-     private Connection conexion;
+    private final Connection conexion;
     private PreparedStatement sentencia;
     private ResultSet resultado;
 
@@ -25,35 +25,19 @@ public class Ingredientes {
         this.conexion = Conexion.getConexion();
     }
     
-    
-    public Ingredientes(String nombre, float cantidadCaloriasEn100gs) {
-        this.nombre = nombre;
-        this.cantidadCaloriasEn100gs = cantidadCaloriasEn100gs;
-    }
-    
-    
-
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public float getCantidadCaloriasEn100gs() {
-        return cantidadCaloriasEn100gs;
-    }
-
-    public void setCantidadCaloriasEn100gs(float cantidadCaloriasEn100gs) {
-        this.cantidadCaloriasEn100gs = cantidadCaloriasEn100gs;
+    public float getCaloriasCda100g() {
+        return caloriasCda100g;
     }
 
     @Override
     public int hashCode() {
         int hash = 7;
         hash = 59 * hash + Objects.hashCode(this.nombre);
-        hash = 59 * hash + Float.floatToIntBits(this.cantidadCaloriasEn100gs);
+        hash = 59 * hash + Float.floatToIntBits(this.caloriasCda100g);
         return hash;
     }
 
@@ -69,7 +53,7 @@ public class Ingredientes {
             return false;
         }
         final Ingredientes other = (Ingredientes) obj;
-        if (Float.floatToIntBits(this.cantidadCaloriasEn100gs) != Float.floatToIntBits(other.cantidadCaloriasEn100gs)) {
+        if (Float.floatToIntBits(this.caloriasCda100g) != Float.floatToIntBits(other.caloriasCda100g)) {
             return false;
         }
         return Objects.equals(this.nombre, other.nombre);
@@ -77,37 +61,30 @@ public class Ingredientes {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Ingredientes{");
-        sb.append("nombre=").append(nombre);
-        sb.append(", cantidadCaloriasEn100gs=").append(cantidadCaloriasEn100gs);
-        sb.append('}');
-        return sb.toString();
+        return "Ingredientes:\nNombre: " + nombre + ".\nCantidad de Calorias (100g): " + caloriasCda100g + ".\n";
     }
     
     //-----Funciones SQL-----------------------------------------------------------------------------------------------------------------------------------
 
     /*Constructor SQL*/
-    
-    public void SQLingrediente (String nombre, float cantidadCaloriasEn100gs){
+    public void SQLingrediente(String nombre, float caloriasCda100g){
         String SQL = "INSERT INTO ingrediente(NombreIng, ValorCD100g) " + 
-                     "VALUES ('" + nombre + "', " + cantidadCaloriasEn100gs + ")";
-        System.out.println(SQL);
+                     "VALUES ('" + nombre + "', " + caloriasCda100g + ")";
         try 
         {
             sentencia = conexion.prepareStatement(SQL);
             int filas = sentencia.executeUpdate();
             
             if (filas > 0) JOptionPane.showMessageDialog(null, "Ingrediente Agregado.");
-
         } 
         catch (SQLException ex) 
         {
             JOptionPane.showMessageDialog(null, "Error en la Sintaxis.");
         }   
     }
- public String getSQLNombre(String nombre) {
-        String SQL = "SELECT nombre FROM Ingrediente WHERE nombreIng=" + nombre;
+    
+    public float getSQLCaloriasCda100g(String nom) {
+        String SQL = "SELECT valorCD100g FROM Ingrediente WHERE NombreI='" + nom + "'";
         try 
         {
             sentencia = conexion.prepareStatement(SQL);
@@ -115,7 +92,7 @@ public class Ingredientes {
             
             while (resultado.next()) 
             {
-                nombre = resultado.getString("nombre");
+                caloriasCda100g = resultado.getFloat("valorCD100g");
             }
         } 
         catch (SQLException ex) 
@@ -123,29 +100,11 @@ public class Ingredientes {
             JOptionPane.showMessageDialog(null, "Error en la Sintaxis.");
         }
         
-        return nombre;
+        return caloriasCda100g;
     }
-public float getSQLcantidadCaloriasEn100gs(float cantidadCaloriasEn100gs) {
-        String SQL = "SELECT valorCD100g FROM Ingrediente WHERE valorCD100G=" + cantidadCaloriasEn100gs;
-        try 
-        {
-            sentencia = conexion.prepareStatement(SQL);
-            resultado = sentencia.executeQuery();
-            
-            while (resultado.next()) 
-            {
-                cantidadCaloriasEn100gs = resultado.getFloat("Calorias por porcion");
-            }
-        } 
-        catch (SQLException ex) 
-        {
-            JOptionPane.showMessageDialog(null, "Error en la Sintaxis.");
-        }
-        
-        return cantidadCaloriasEn100gs;
-}
-        public void setSQLcantidadCaloriasEn100gs(float cantidadCaloriasEn100gs) {
-        String SQL = "UPDATE Ingrediente SET valorCD100G='" + cantidadCaloriasEn100gs ;
+    
+    public void setCaloriasCda100g(float caloriasCda100g, String nom) {
+        String SQL = "UPDATE Ingrediente SET valorCD100G=" + caloriasCda100g + "WHERE NombreI='" + nom + "'";
         try 
         {
             sentencia = conexion.prepareStatement(SQL);
@@ -158,15 +117,4 @@ public float getSQLcantidadCaloriasEn100gs(float cantidadCaloriasEn100gs) {
             JOptionPane.showMessageDialog(null, "Error en la Sintaxis.");
         }    
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
-
-
